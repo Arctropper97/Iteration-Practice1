56243b2e9310a74550da8f391897b55c90ba5307
Kenneth Ellington <kellington1@usf.edu>
2020-02-09 10:45:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Iteration_Practice1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 10;
+            int i;
+            
+
+            for (i = start; i <= end; i++)
+            {
+                Console.WriteLine(i + " ");
+            }
+
+            //We are declaring the variables for our while loop
+            int end1 = 10;
+            int count = 1;
+            
+
+            while(count <= end1)
+            {
+                Console.WriteLine(count + " ");
+                count++;  
+            }
+            Console.WriteLine("You did it!");
+            
+        }
+    }
+}
+
+
